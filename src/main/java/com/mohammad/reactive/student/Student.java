029e78a574897cba28a51619853e79530491ddc5
Mohammad Alabed <mohammad.alabed@inspirejo.com>
2024-09-22 12:07:50 +0300

    initial commit

---
+++ src/main/java/com/mohammad/reactive/student/Student.java
@@ -0,0 +1,20 @@
+package com.mohammad.reactive.student;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "students")
+public class Student {
+
+    @Id
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private int age;
+}
